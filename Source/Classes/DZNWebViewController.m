8ce1c83cc1d41c3ccb741f5da21a2c80e90f33ac
iorikingdom <iorikingdom@hotmail.com>
2015-01-08 00:30:18 +0900

    Update DZNWebViewController.m
    change _loadingProcess to self. loadingProcess

--- Source/Classes/DZNWebViewController.m
+++ Source/Classes/DZNWebViewController.m
@@ -664,7 +664,7 @@ static char DZNWebViewControllerKVOContext = 0;
         }];
     }
     
-    _loadingProcess = progress;
+    self.loadingProcess = progress;
     [self.progressView setProgress:progress animated:YES];
 }
 
